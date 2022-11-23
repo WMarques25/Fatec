136b0a3c79099fdf87c716c33aa51cbbf5b6e947
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2022-11-23 15:56:37 -0400

    inicio

--- Desafio Arduino2.ino
+++ Desafio Arduino2.ino
@@ -3,3 +3,14 @@
 // https://www.tinkercad.com/things/7WQPClX3da9?sharecode=dLqwzGIEuqbS1qPxSSYnwXeQ9D6Y3mlDx9duF33hcdk
 
 #include <Adafruit_LiquidCrystal.h>
+  int led1 = 13; 
+  int led2 = 10;
+  int led3 = 9;
+  int BT1 = 7;
+  int BT2 = 6;
+  int buzz = 8;
+  int LDR = A0;
+  int LM35 = A1;
+  int Pot = A2; 
+  unsigned int valorPot; 
+  unsigned int pwm;
