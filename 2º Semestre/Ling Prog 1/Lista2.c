e0b7260ac40c3233a308349dca85929fa437d183
Your Name <you@example.com>
2023-03-07 20:09:17 -0300

    1

--- 2º Semestre/Ling Prog 1/Lista2.c
+++ 2º Semestre/Ling Prog 1/Lista2.c
@@ -3,199 +3,186 @@
 #include <ctype.h>
 
 #ifdef ex1
-int main(void)
-{
-    char c;
-    int h1, h2, m1, m2, s1, s2;
-
-    do
+    int main(void)
     {
-        system("cls");
-        printf("Insira a quantidade de horas do primeiro instante: ");
-        scanf(" %d", &h1);
-        printf("Insira a quantidade de minutos do primeiro instante: ");
-        scanf(" %d", &m1);
-        printf("Insira a quantidade de segundos do primeiro instante: ");
-        scanf(" %d", &s1);
+        char c;
+        int h1, h2, m1, m2, s1, s2;
 
-        system("cls");
-        printf("Insira a quantidade de horas do segundo instante: ");
-        scanf(" %d", &h2);
-        printf("Insira a quantidade de minutos do segundo instante: ");
-        scanf(" %d", &m2);
-        printf("Insira a quantidade de segundos do segundo instante: ");
-        scanf(" %d", &s2);
+        do
+        {
+            system("cls");
+            printf("Insira a quantidade de horas do primeiro instante: ");
+            scanf(" %d", &h1);
+            printf("Insira a quantidade de minutos do primeiro instante: ");
+            scanf(" %d", &m1);
+            printf("Insira a quantidade de segundos do primeiro instante: ");
+            scanf(" %d", &s1);
 
-        h1 *= 3600;
-        m1 *= 60;
-        s1 = s1 + m1 + h1;
+            system("cls");
+            printf("Insira a quantidade de horas do segundo instante: ");
+            scanf(" %d", &h2);
+            printf("Insira a quantidade de minutos do segundo instante: ");
+            scanf(" %d", &m2);
+            printf("Insira a quantidade de segundos do segundo instante: ");
+            scanf(" %d", &s2);
 
-        h2 *= 3600;
-        m2 *= 60;
-        s2 = s2 + m2 + h2;
+            h1 *= 3600;
+            m1 *= 60;
+            s1 = s1 + m1 + h1;
 
-        s1 = s2 - s1;
-        h1 = s1 / 3600;
-        m1 = (s1 % 3600) / 60;
-        s1 = (s1 % 3600) % 60;
+            h2 *= 3600;
+            m2 *= 60;
+            s2 = s2 + m2 + h2;
 
-        printf("A duracao da ligacao eh de: %d horas %d minutos %d segundos.", h1, m1, s1);
+            s1 = s2 - s1;
+            h1 = s1 / 3600;
+            m1 = (s1 % 3600) / 60;
+            s1 = (s1 % 3600) % 60;
 
-        printf("\nDeseja continuar? ('S' / 'N')\n");
-        scanf(" %c", &c);
-        c = toupper(c);
+            printf("A duracao da ligacao eh de: %d horas %d minutos %d segundos.", h1, m1, s1);
 
-    } while (c == 'S');
-}
+            printf("\nDeseja continuar? ('S' / 'N')\n");
+            scanf(" %c", &c);
+            c = toupper(c);
+
+        } while (c == 'S');
+    }
 #endif // ex1
 #ifdef ex2
-int main(void)
-{
-    char c, s = 'x';
-    do
+    int main(void)
     {
-        int min = 1, max = 99, chute;
-        printf("Pense em um numero entre 1 e 99.\n");
-        system("pause");
-
-        while (s != '=')
+        char c, s = 'x';
+        do
         {
-            chute = (min + max) / 2;
-            printf("O numero eh %d? ('=' / '>' / '<')\n", chute);
-            scanf(" %c", &s);
+            int min = 1, max = 99, chute;
+            printf("Pense em um numero entre 1 e 99.\n");
+            system("pause");
 
-            if (s == '=')
+            while (s != '=')
             {
-                printf("Acertei!!\n");
-            }
-            else if (s == '<')
-            {
-                max = chute;
-            }
-            else if (s == '>')
-            {
-                min = chute;
-            }
-            else
-            {
-                printf("Opcao invalida\n");
-            }
-        }
+                chute = (min + max) / 2;
+                printf("O numero eh %d? ('=' / '>' / '<')\n", chute);
+                scanf(" %c", &s);
 
-        printf("\nDeseja continuar? ('S' / 'N')\n");
-        scanf(" %c", &c);
-        c = toupper(c);
+                if (s == '=')
+                {
+                    printf("Acertei!!\n");
+                }
+                else if (s == '<')
+                {
+                    max = chute;
+                }
+                else if (s == '>')
+                {
+                    min = chute;
+                }
+                else
+                {
+                    printf("Opcao invalida\n");
+                }
+            }
 
-    } while (c == 'S');
-}
+            printf("\nDeseja continuar? ('S' / 'N')\n");
+            scanf(" %c", &c);
+            c = toupper(c);
+
+        } while (c == 'S');
+    }
 #endif // ex2
 #ifdef ex3
-int main(void)
-{
-    char c, s = 'x';
-    do
+    int main(void)
     {
-        int min = 1, max = 99, chute, tentativa = 0;
-        printf("Pense em um numero entre 1 e 99.\n");
-        system("pause");
-
-        while (s != '=')
+        char c, s = 'x';
+        do
         {
-            tentativa++;
-            chute = (min + max) / 2;
-            printf("O numero eh %d? ('=' / '>' / '<')\n", chute);
-            scanf(" %c", &s);
+            int min = 1, max = 99, chute, tentativa = 0;
+            printf("Pense em um numero entre 1 e 99.\n");
+            system("pause");
 
-            switch (s)
+            while (s != '=')
             {
-            case '=':
-                printf("Acertei!!\nTentativas: %d", tentativa);
-                break;
+                tentativa++;
+                chute = (min + max) / 2;
+                printf("O numero eh %d? ('=' / '>' / '<')\n", chute);
+                scanf(" %c", &s);
 
-            case '<':
-                max = chute;
-                break;
+                switch (s)
+                {
+                case '=':
+                    printf("Acertei!!\nTentativas: %d", tentativa);
+                    break;
 
-            case '>':
-                min = chute;
-                break;
+                case '<':
+                    max = chute;
+                    break;
 
-            default:
-                printf("Opcao invalida\n");
+                case '>':
+                    min = chute;
+                    break;
+
+                default:
+                    printf("Opcao invalida\n");
+                }
             }
-        }
 
-        printf("\nDeseja continuar? ('S' / 'N')\n");
-        scanf(" %c", &c);
-        c = toupper(c);
+            printf("\nDeseja continuar? ('S' / 'N')\n");
+            scanf(" %c", &c);
+            c = toupper(c);
 
-    } while (c == 'S');
-}
+        } while (c == 'S');
+    }
 #endif // ex3
 #ifdef ex4
 
-int main(void)
-{
-
-    char c;
-
-    do
+    int main(void)
     {
-        int i = 0, n = 0, soma = 0;
-        float med;
 
-        while (n >= 0)
+        char c;
+
+        do
         {
+            int i = 0, n = 0, soma = 0;
+            float med;
 
-            printf("Digite um numero positivo: ");
-            scanf(" %d", &n);
+            while (n >= 0)
+            {
 
-            if (n < 0)
-                break;
+                printf("Digite um numero positivo: ");
+                scanf(" %d", &n);
 
-            soma += n;
-            i++;
-        }
-        med = (float)soma / (float)i;
-        printf("A media dos %d eh %.2f", i, med);
+                if (n < 0)
+                    break;
 
-        printf("\nDeseja continuar? ('S' / 'N')\n");
-        scanf(" %c", &c);
-        c = toupper(c);
+                soma += n;
+                i++;
+            }
+            med = (float)soma / (float)i;
+            printf("A media dos %d eh %.2f", i, med);
 
-    } while (c == 'S');
-}
+            printf("\nDeseja continuar? ('S' / 'N')\n");
+            scanf(" %c", &c);
+            c = toupper(c);
+
+        } while (c == 'S');
+
+        return 0;
+    }
 #endif // ex4
 #ifdef ex5
-int main(void)
-{
-
-    char c;
-
-    do
+    int main(void)
     {
-        int i = 0, n = 0, soma = 0;
-        float med;
+        char c;
 
-        while (n >= 0)
+        do
         {
+            
 
-            printf("Digite um numero positivo: ");
-            scanf(" %d", &n);
+            printf("\nDeseja continuar? ('S' / 'N')\n");
+            scanf(" %c", &c);
+            c = toupper(c);
 
-            if (n < 0)
-                break;
+        } while (c == 'S');
 
-            soma += n;
-            i++;
-        }
-        med = (float)soma / (float)i;
-        printf("A media dos %d eh %.2f", i, med);
-
-        printf("\nDeseja continuar? ('S' / 'N')\n");
-        scanf(" %c", &c);
-        c = toupper(c);
-
-    } while (c == 'S');
-}
+        return 0;
+    }
 #endif // ex5
