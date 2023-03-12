5d0f66ff3f77f3274fd6fc6c87bdb26e8e039fbc
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2023-03-12 18:21:08 -0300

    update Lista3.c

--- 2º Semestre/Ling Prog 1/Lista3.c
+++ 2º Semestre/Ling Prog 1/Lista3.c
@@ -74,13 +74,13 @@
             v2 = verifica(y);
 
             if(v1 == 1 && v2 == 1){
-                printf("A soma dos numeros entre %d e %d e´ %d", x, y, soma(x, y));
+                printf("A soma dos numeros entre %d e %d eÂ´ %d", x, y, soma(x, y));
             }
             else if (v1 == 0 && v2 == 0){
-                printf("A multiplicacao dos numeros entre %d e %d e´ %d", x, y, mult(x, y));
+                printf("A multiplicacao dos numeros entre %d e %d eÂ´ %d", x, y, mult(x, y));
             }
             else if((v1 == 1 && v2 == 0) || (v1 == 0 && v2 == 1)){
-                printf("A divisao dos numeros entre %d e %d e´ %f", x, y, div(x, y));
+                printf("A divisao dos numeros entre %d e %d eÂ´ %f", x, y, div(x, y));
             }
 
 
@@ -96,18 +96,29 @@
 #endif // ex1
 #ifdef ex2
     int func(int x){
+        int soma = 0;
         // for de fatorial
         for(int i=x-1 ; i>1 ; i--){
             x *= i;
             printf(" %d", x); // remover
         }
         // for de soma dos alg (y = x % 10; soma += y; x = x/10)
+        while(x>0){
+            soma += x%10;
+            x /= 10;
+        }
+        return soma;
     }
 
     int main(void){
         char c;
+        int x;
 
         do{
+            printf("Digite um numero: ");
+            scanf(" %d",&x);
+            
+            printf("A soma dos algarismos do fatorial de %d eh: %d",x, func(x));
 
             printf("\nDeseja continuar? ('S' / 'N')\n");
             scanf(" %c", &c);
