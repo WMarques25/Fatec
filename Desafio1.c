eb159534ddaa779bc5b67813de507adcdabcf108
Wellington <WMarques25>
2023-04-04 21:24:56 -0300

    .

--- Desafio1.c
+++ Desafio1.c
@@ -11,11 +11,25 @@ char autor2[20];
 char autor3[20];
 
 // Funções do Menu
-void Entrada(){}
+void Entrada(){
+    printf("Entre com o nome do primeiro livro:");
+    gets(livro1);
+    printf("%s\n", livro1);
+
+    printf("Entre com o nome do autor:");
+    gets(autor1);
+
+    printf("Entre com o nome do segundo livro:");
+    gets(livro2);
+    printf("%s\n", livro2);
+
+    printf("Entre com o nome do autor:");
+    gets(autor2);
+}
 
 void Listar(){}
 
-void PesquisaLivor(){}
+void PesquisaLivro(){}
 
 void PesquisaAutor(){}
 
@@ -28,6 +42,7 @@ void Sair(){}
 int main(void){
 
     char c = 'a';
+    int op;
     setlocale(LC_ALL, "");
 
     do{
@@ -35,14 +50,50 @@ int main(void){
         // print menu
         printf(" * - Biblioteca - *\n\n");
         printf(" 1 - Entrar com os livros.\n");
-        printf(" 2 - Listar os livros.");
-        printf(" 3 - Pesquisar por livro.");
-        printf(" 4 - Pesquisar por autor.");
-        printf(" 5 - Alterar livro.");
-        printf(" 6 - Excluir livro.");
-        printf(" 7 - Sair.");
+        printf(" 2 - Listar os livros.\n");
+        printf(" 3 - Pesquisar por livro.\n");
+        printf(" 4 - Pesquisar por autor.\n");
+        printf(" 5 - Alterar livro.\n");
+        printf(" 6 - Excluir livro.\n");
+        printf(" 7 - Sair.\n\n");
+        printf("Insira a opção desejada: ");
+        scanf(" %d", &op);
+        getchar();
 
         // switch case
+        switch(op){
+            case 1:
+                Entrada();
+                break;
+            
+            case 2:
+                Listar();
+                break;
+            
+            case 3:
+                PesquisaLivro();
+                break;
+
+            case 4:
+                PesquisaAutor();
+                break;
+            
+            case 5:
+                AlteraDados();
+                break;
+            
+            case 6:
+                ExcluirDados();
+                break;
+
+            case 7:
+                Sair();
+                break;
+
+            default:
+                printf("\nOpção Inválida.\n");
+                break;
+        }
 
         //  
     }while(c != 'S');
