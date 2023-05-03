ef682ad68baea0603afd282d4b6f6c838a4bdf3f
Rhayadh Zaparoli <120688001+rhayadhzaparoli@users.noreply.github.com>
2023-05-02 21:01:32 -0300

    lista8

---
+++ 2º Semestre/Ling Prog 1/Lista8.c
@@ -0,0 +1,24 @@
+#define ex1
+
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+
+#ifdef  ex1
+
+#endif  //ex1
+#ifdef  ex2
+
+#endif  //ex2
+#ifdef  ex3
+
+#endif  //ex3
+#ifdef  ex4
+
+#endif  //ex4
+#ifdef  ex5
+
+#endif  //ex5
+#ifdef  ex6
+
+#endif  //ex6
