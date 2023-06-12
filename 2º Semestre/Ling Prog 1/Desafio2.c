f0fe85bed44c7d79d99e6216d3ee5ab0961852b4
W <ti.wmarques@outlook.com>
2023-06-12 12:41:31 -0300

    Desafio Pronto

--- 2º Semestre/Ling Prog 1/Desafio2.c
+++ 2º Semestre/Ling Prog 1/Desafio2.c
@@ -15,6 +15,7 @@ void Inserir(Livro *livros){
     FILE *arq;
     char pesq[50];
     char *n1, *n2;
+    int i = 0;
 
     system("cls");
     arq = fopen("livros.txt", "r+");
@@ -25,6 +26,7 @@ void Inserir(Livro *livros){
         n2 = &livros->titulo;
 
         if(livros->titulo[0] == '*'){
+            getc(arq);
             continue;
         }
 
@@ -41,17 +43,26 @@ void Inserir(Livro *livros){
         }
 
     }
+    
+    while(pesq[i] != '\0'){
+        livros->titulo[i] = pesq[i];
+        i++;
+        if(pesq[i] == '\0'){
+            livros->titulo[i] = '\0';
+        }
+    }
 
-    *livros->titulo = *pesq;
+    fseek(arq, 0, SEEK_END);
     printf("Insira o autor do livro: ");
     gets(livros->autor);
     printf("Insira o preço do livro: ");
-    scanf(" %f", &livros->preco);
+    scanf("%f", &livros->preco);
     getchar();
     printf("Insira a quantidade do livro: ");
-    scanf(" %d", &livros->quantidade);
+    scanf("%d", &livros->quantidade);
     getchar();
     fwrite(livros, sizeof(*livros), 1, arq);
+    putc('\n', arq);
     fclose(arq);
     printf("Livro cadastrado com sucesso!\n");
     system("pause");
@@ -61,6 +72,7 @@ void Listar(Livro *livros){
     FILE *arq;
     int i = 0;
     arq = fopen("livros.txt", "r");
+    system("cls");
     while(fread(livros, sizeof(*livros), 1, arq) != 0){
         if(livros->titulo[0] == '*'){
             continue;
@@ -72,6 +84,7 @@ void Listar(Livro *livros){
         printf("Preço: %.2f\n", livros->preco);
         printf("Quantidade: %d\n", livros->quantidade);
         printf("\n");
+        getc(arq);
     }
     fclose(arq);
     system("pause");
@@ -90,6 +103,7 @@ void Pesquisar(Livro *livros){
         n2 = &livros->titulo;
 
         if(livros->titulo[0] == '*'){
+            getc(arq);
             continue;
         }
 
@@ -109,6 +123,7 @@ void Pesquisar(Livro *livros){
             }
 
         }
+        getc(arq);
 
     }
     printf("Livro não encontrado!\n");
@@ -130,6 +145,7 @@ void PesquisarAutor(Livro *livros){
         n2 = &livros->autor;
 
         if(livros->titulo[0] == '*'){
+            getc(arq);
             continue;
         }
 
@@ -147,7 +163,7 @@ void PesquisarAutor(Livro *livros){
             }
 
         }
-
+        getc(arq);
     }
 
     if(i == 0){
@@ -165,14 +181,15 @@ void PesquisarPreco(Livro *livros){
     system("cls");
     arq = fopen("livros.txt", "r");
     printf("Insira o preço minimo: ");
-    scanf(" %f", &min);
+    scanf("%f", &min);
     getchar();
     printf("Insira o preço maximo: ");
-    scanf(" %f", &max);
+    scanf("%f", &max);
     getchar();
 
     while(fread(livros, sizeof(*livros), 1, arq) != 0){
         if(livros->titulo[0] == '*'){
+            getc(arq);
             continue;
         }
 
@@ -185,6 +202,7 @@ void PesquisarPreco(Livro *livros){
             printf("Quantidade: %d\n", livros->quantidade);
             printf("\n");
         }
+        getc(arq);
     }
 
     if(i == 0){
@@ -209,6 +227,7 @@ void AlterarQuantidade(Livro *livros){
         n2 = &livros->titulo;
 
         if(livros->titulo[0] == '*'){
+            getc(arq);
             continue;
         }
 
@@ -235,7 +254,7 @@ void AlterarQuantidade(Livro *livros){
             }
 
         }
-
+        getc(arq);
     }
     printf("Livro não encontrado!\n");
     fclose(arq);
@@ -256,6 +275,7 @@ void AlterarPreco(Livro *livros){
         n2 = &livros->titulo;
 
         if(livros->titulo[0] == '*'){
+            getc(arq);
             continue;
         }
 
@@ -282,19 +302,123 @@ void AlterarPreco(Livro *livros){
             }
 
         }
+        getc(arq);
 
     }
     printf("Livro não encontrado!\n");
     fclose(arq);
     system("pause");
 }
-void AlterarDados(Livro *livros){}
-void ExcluirLivro(Livro *livros){}
+void AlterarDados(Livro *livros){
+    FILE *arq;
+    char pesq[50];
+    char *n1, *n2;
+
+    system("cls");
+    arq = fopen("livros.txt", "r+");
+    printf("Insira o titulo do livro: ");
+    gets(pesq);
+
+    while(fread(livros, sizeof(*livros), 1, arq) != 0){
+        n1 = &pesq;
+        n2 = &livros->titulo;
+
+        if(livros->titulo[0] == '*'){
+            getc(arq);
+            continue;
+        }
+
+        while(*n1 == *n2 && *n1 != '\0' && *n2 != '\0'){
+            n1++;
+            n2++;
+            if(*n1 == '\0' && *n2 == '\0'){
+                printf("Livro encontrado!\n");
+                printf("Titulo: %s\n", livros->titulo);
+                printf("Autor: %s\n", livros->autor);
+                printf("Preço: %.2f\n", livros->preco);
+                printf("Quantidade: %d\n", livros->quantidade);
+                printf("\n");
+                printf("Insira o novo titulo: ");
+                gets(livros->titulo);
+                printf("Insira o novo autor: ");
+                gets(livros->autor);
+                printf("Insira o novo preço: ");
+                scanf(" %f", &livros->preco);
+                getchar();
+                printf("Insira a nova quantidade: ");
+                scanf(" %d", &livros->quantidade);
+                getchar();
+                fseek(arq, -sizeof(*livros), SEEK_CUR);
+                fwrite(livros, sizeof(*livros), 1, arq);
+                fclose(arq);
+                printf("Dados alterados com sucesso!\n");
+                system("pause");
+                return;
+            }
+
+        }
+        getc(arq);
+
+    }
+}
+void ExcluirLivro(Livro *livros){
+    FILE *arq;
+    char pesq[50];
+    char *n1, *n2;
+
+    system("cls");
+    arq = fopen("livros.txt", "r+");
+    printf("Insira o titulo do livro: ");
+    gets(pesq);
+
+    while(fread(livros, sizeof(*livros), 1, arq) != 0){
+        n1 = &pesq;
+        n2 = &livros->titulo;
+
+        if(livros->titulo[0] == '*'){
+            getc(arq);
+            continue;
+        }
+
+        while(*n1 == *n2 && *n1 != '\0' && *n2 != '\0'){
+            n1++;
+            n2++;
+            if(*n1 == '\0' && *n2 == '\0'){
+                printf("Livro encontrado!\n");
+                printf("Titulo: %s\n", livros->titulo);
+                printf("Autor: %s\n", livros->autor);
+                printf("Preço: %.2f\n", livros->preco);
+                printf("Quantidade: %d\n", livros->quantidade);
+                printf("\n");
+                livros->titulo[0] = '*';
+                fseek(arq, -sizeof(*livros), SEEK_CUR);
+                fwrite(livros, sizeof(*livros), 1, arq);
+                fclose(arq);
+                printf("Livro excluido com sucesso!\n");
+                system("pause");
+                return;
+            }
+
+        }
+        getc(arq);
+
+    }
+    printf("Livro não encontrado!\n");
+    fclose(arq);
+    system("pause");
+}
 
 int main(void){
     Livro livros;
     setlocale(LC_ALL, "PORTUGUESE");
     int op;
+    FILE *arq;
+
+    if((arq = fopen("livros.txt", "r")) == NULL){
+        fclose(arq);
+        arq = fopen("livros.txt", "w");
+        fclose(arq);
+    }
 
     while(1){
         system("cls");
@@ -309,7 +433,7 @@ int main(void){
         printf(" 9 - Excluir livro\n");
         printf("10 - Sair\n");
         printf("Insira a opção: ");
-        scanf(" %d", &op);
+        scanf("%d", &op);
         getchar();
 
         switch(op){
